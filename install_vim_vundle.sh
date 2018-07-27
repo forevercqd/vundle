5b6ddcee773d764c2c589bae7ceea3bc381e8651
Robert <qingdong.cqd@alibaba-inc.com>
2018-07-27 10:52:42 +0800

    1. 去掉目前Mac 平台存在不支持的插件; 2. 修改安装脚本;

--- install_vim_vundle.sh
+++ install_vim_vundle.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-sudo apt-get install -y cscope
+# code url:	 https://github.com/forevercqd/vundle.git ~/vundle
+ROOT_DIR=`pwd`
+sudo brew install -y cscope
 
 cd ~/
-sudo git clone https://github.com/forevercqd/vundle.git ~/vundle
-sudo cp -rf  ~/vundle/.vim*  ~/
+# sudo git clone https://github.com/forevercqd/vundle.git ~/vundle
+sudo cp -rf  $ROOT_DIR/.vim*  ~/
 sudo rm -rf ~/.vim/bundle/Vundle.vim
 sudo git clone   https://github.com/gmarik/Vundle.vim.git    ~/.vim/bundle/Vundle.vim
 sudo vim
